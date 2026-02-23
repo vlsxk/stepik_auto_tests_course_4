6844e20446537560ce4ab9010000cbc1bb585268
Vlsxk <d.chirkov@magnus-tech.ru>
2026-02-23 18:50:37 +0300

    коммит 4.2 9/12

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#1login_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
     LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
