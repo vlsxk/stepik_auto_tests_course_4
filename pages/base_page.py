9fba51d120877477ba64b5e7adbe4ae51b8f5fb9
Vlsxk <d.chirkov@magnus-tech.ru>
2026-02-25 01:40:43 +0300

    Модуль 4.3, задание 13/15

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,7 +14,7 @@ class BasePage():
         self.browser.get(self.url)
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
